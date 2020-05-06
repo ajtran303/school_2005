0e93f4ebbda7bf95ec55ec75437e581189797080
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-06 13:12:51 -0600

    Complete iteration 1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
+class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
 
+  def initialize(time, hrs)
+    @start_time = time
+    @hours_in_school_day = hrs
+    @student_names = []
+  end
+
+end
