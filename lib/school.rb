d934d8c14c64a53c3ae7cb6fa978739e26d971f9
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-06 13:22:33 -0600

    Add passing iteration 2

--- lib/school.rb
+++ lib/school.rb
@@ -13,4 +13,11 @@ class School
     @student_names << student
   end
 
+  def end_time
+    start = @start_time.gsub(":", ".").to_f
+    hours = @hours_in_school_day * 1.00
+    finish = start + hours
+    "#{finish}0".gsub(".", ":")
+  end
+
 end
